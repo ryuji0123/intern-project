2bc0c73f7f5be2569e6a65de5b94d725cb3bc985
Ryou Okuda <ryouokuda@okudas-MacBook-Pro.local>
2020-03-04 17:12:09 +0900

    add code to detect pressing left or right arrow

--- public/js/process_json.js
+++ public/js/process_json.js
@@ -7,11 +7,10 @@ window.onload = function()
 		createSliderElement($('div#img-slider-container'), data);
 		$('div.slide').click(function() {
 			let time_to_seek = $(this).attr('data-seek-time');
-			if (time_to_seek === undefined) {
+			if (time_to_seek === undefined || time_to_seek === 'empty') {
 				return;
 			}
 			player.seekTo(time_to_seek);
-			console.log(time_to_seek);
 		});
 		
 		function parseAndShowJson(data) 
@@ -25,12 +24,68 @@ window.onload = function()
 		{
 			for (let i = 0; i < number_of_sample_images; i++) {
 				let current_img_path = SAMPLE_IMG_BASE_PATH + 'sample' + i + '.png';
-				$("<div class='slide' data-seek-time=" + data[i] + "><img src=" + current_img_path  + "></div>").appendTo(parent_div);
+				let child_div = $("<div class='slide' data-seek-time=" + data[i] + "><img src=" + current_img_path  + "></div>")
+				if (data.length <= i) {
+					child_div.attr('data-seek-time', 'empty');
+				}
+				child_div.appendTo(parent_div);
 			}
 		}
+
+		let slide_index = 1;
+    	showSlides();
+    	$('a.prev').click(function() {
+    		plusSlides(-1)
+		});
+		$('a.next').click(function() {
+			plusSlides(1)
+		});
+
+
+		document.onkeydown= function (e) {
+			e = e || window.event;
+			if (e.keyCode === 37) {
+				plusSlides(-1);
+			} else if (e.keyCode === 39) {
+				plusSlides(1);
+			}
+		}
+
+
+    	function showSlides() {
+    	    let i;
+    	    let slides = document.getElementsByClassName('slide');
+			//let dots = document.getElementsByClassName('dot');
+    	    if (slide_index > slides.length) {
+    	        slide_index = 1;
+    	    }
+    	    if (slide_index < 1) {
+    	        slide_index = slides.length;
+    	    }
+
+    	    for (i = 0; i < slides.length; i++) {
+    	        slides[i].style.display = 'none';
+    	    }
+
+    	    //for (i = 0; i < dots.length; i++) {
+    	    //    dots.className = dots[i].className.replace('active', '');
+    	    //}
+    	    slides[slide_index - 1].style.display = 'block';
+    	    //dots[slide_index - 1].className += ' active';
+    	}
+
+		function currentSlide(n)
+		{
+			showSlides();
+		}
+
+		function plusSlides(n)
+		{
+			slide_index += n
+			showSlides();
+		}
+
 	});
-	
-	
 };
 
 
